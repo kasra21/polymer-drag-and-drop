1ca3236fdf0291376edb02be08f95e046dd22989
kasra kazemi <kasra.kazemi@scientificgames.com>
2020-05-03 21:03:13 -0700

    added some styling

--- polymer-drag-and-drop.js
+++ polymer-drag-and-drop.js
@@ -16,6 +16,25 @@ class PolymerDragAndDrop extends PolymerElement {
         :host {
           display: block;
         }
+        ::slotted(.drag-container) {
+          display: block;
+          background-color: #dfdfdf;
+          border-radius: 5px;
+          min-height: 50px;
+          margin: 0 auto;
+          padding: 2em;
+          text-align: center;
+        }
+        ::slotted(.drop-container) {
+          border: 2px dashed #D9D9D9;
+          border-radius: 5px;
+          min-height: 50px;
+          margin: 0 auto;
+          margin-top: 10px;
+          padding: 2em;
+          display: block;
+          text-align: center;
+        }
       </style>
       <slot></slot>
     `;
@@ -52,10 +71,10 @@ class PolymerDragAndDrop extends PolymerElement {
   constructor() {
     super();    
     var containers = [this.querySelector(this.dragContainerSelector),this.querySelector(this.dropContainerSelector)];
-
     this.dragulaObj = dragula(containers, {
       revertOnSpill: true
     });
+    
   }
 
 }
